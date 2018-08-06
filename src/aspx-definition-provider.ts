2921a9f3391a206334c1d106334fb59e68b050a0
Nathan Teague <nteague@summitracing.com>
2018-08-06 10:09:08 -0400

    Made the interpolated aspx text parse better and corrected path issues

--- src/aspx-definition-provider.ts
+++ src/aspx-definition-provider.ts
@@ -1,66 +1,53 @@
-import {DefinitionProvider, TextDocument, Position, CancellationToken, Location, workspace, Uri, window, StatusBarAlignment, OutputChannel, StatusBarItem} from "vscode";
+import {DefinitionProvider, TextDocument, Position, CancellationToken, Location, Uri, window, StatusBarAlignment, OutputChannel, StatusBarItem, WorkspaceConfiguration, workspace} from "vscode";
 import * as path from 'path';
 
-export const renderPartialPattern = /Html\.RenderPartial\(\"([A-Za-z0-9-_]+[A-Za-z0-9-_\/]*)\".*\)/i;
-
 export default class AspxDefinitionProvider implements DefinitionProvider {
+    resultChain: Thenable<Location>;
+
     SharedPaths: string[];
 
     readonly log: OutputChannel;
 
     status: StatusBarItem;
 
-    constructor(log: OutputChannel) {
-        this.SharedPaths = ['Views/Shared'];
+    config: WorkspaceConfiguration;
+
+    constructor(log: OutputChannel, config: WorkspaceConfiguration) {
+        this.SharedPaths = config.has("sharedPaths") ? config.get<string[]>("sharedPaths") : ['Views/Shared'];
         this.log = log;
-        this.status = window.createStatusBarItem(StatusBarAlignment.Right, -1);
+        this.config = config;
     }
 
     provideDefinition(document: TextDocument, position: Position, token: CancellationToken): Location | Location[] | Thenable<Location | Location[]> {
-        this.log.clear();
-        this.status.text = "$(zap) Parsing Request";
-        let selectedRange = document.getWordRangeAtPosition(position, renderPartialPattern/*/\"[A-Za-z0-9-_\/]+\"/*/);
+        let renderPartialPattern = /(?:RenderPartial\(\")([\w\/~]+)(\")/i;
+        let selectedRange = document.getWordRangeAtPosition(position, renderPartialPattern);
         if (selectedRange && !selectedRange.isEmpty) {
-            this.log.appendLine(`matched ${selectedRange.start.character} to ${selectedRange.end.character} in ${document.uri.path}`);
-            let selection = renderPartialPattern.exec(document.getText(selectedRange));
-            this.log.appendLine(`Yielded path of ${selection[1]}`);
+            let selection = renderPartialPattern.exec(document.getText(selectedRange))[1];
+            
             if (!selection) {
                 return null;
             }
-            
-            let baseFolder = workspace.getWorkspaceFolder(document.uri);
-            this.log.appendLine(`found workspace folder as ${baseFolder.uri}`);
-
-            let basePath: string = baseFolder.uri.fsPath.indexOf(baseFolder.name) > -1 ? baseFolder.name : baseFolder.uri.path.split('/').reverse().pop();
-            this.log.appendLine(`found base path to be ${basePath}`);
-
-            //let config = workspace.getConfiguration('aspnetViews');
-            // console.log(config);
-            /* if(config && config.has('sharedPaths')) this.SharedPaths = config.get<string[]>('sharedPaths'); */
-            let localPath = workspace.asRelativePath(document.uri);
-            this.log.appendLine(`The requesting document relative path: ${localPath}`);
-            let localPathChunks = localPath.split(path.sep);
-            localPathChunks.pop();
+            let localPath = path.dirname(workspace.asRelativePath(document.uri, true)).split(path.sep);
             
             let searchPaths: string[] = [];
-            searchPaths.push(`${localPathChunks.join('/')}/${selection[1]}.ascx`);
-            this.log.appendLine(`Searching ${localPathChunks.join('/')}/${selection[1]}.ascx`);
+            searchPaths.push(`${localPath.join('/')}/${selection}.ascx`);
+            this.log.appendLine(`Searching ${localPath.join('/')}/${selection}.ascx`);
             
             for (let p of this.SharedPaths) {
-                this.log.appendLine(`Searching ${p}/${selection[1]}.ascx`);
-                searchPaths.push(`${p}/${selection[1]}.ascx`);
+                this.log.appendLine(`Searching ${p}/${selection}.ascx`);
+                searchPaths.push(`${p}/${selection}.ascx`);
             }
             let resultChain: Thenable<Location> = null;
 
             for (let res of searchPaths) {
                 if (!resultChain) {
-                    resultChain = this.searchPath(res)
+                    resultChain = this.searchPath(res);
                 } else {
                     resultChain = resultChain.then((r) => {
                         if (r && r.uri.path) {
                             return r;
                         }
-                        return this.searchPath(res)
+                        return this.searchPath(res);
                     });
                 }
             }
--- src/extension.ts
+++ src/extension.ts
@@ -6,14 +6,17 @@ const razor_file: vscode.DocumentFilter = { language: 'razor', scheme: 'file' };
 const aspx_file: vscode.DocumentFilter = { language: 'aspx', scheme: 'file' };
 // this method is called when your extension is activated
 
+export const log = vscode.window.createOutputChannel("ASP.Net View Tools");
+
 export function activate(context: vscode.ExtensionContext) {
-    const log = vscode.window.createOutputChannel("ASP.Net View Tools");
     context.subscriptions.push(
         log,
-        vscode.languages.registerDefinitionProvider(razor_file, new RazorDefinitionProvider(log)),
-        vscode.languages.registerDefinitionProvider(aspx_file, new AspxDefinitionProvider(log))
+        // vscode.languages.registerDefinitionProvider(razor_file, new RazorDefinitionProvider(log)),
+        vscode.languages.registerDefinitionProvider(aspx_file, new AspxDefinitionProvider(log, vscode.workspace.getConfiguration("aspnetViews")))
     );
 }
+
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {
